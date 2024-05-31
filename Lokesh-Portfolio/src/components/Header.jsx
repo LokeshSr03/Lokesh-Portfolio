8a0eb8203185e5e237ba798dd5980a7438c5c693
LokeshSr03 <lokeshsuthar0123@gmail.com>
2024-05-31 19:24:44 +0530

    Dark mode logic added

--- Lokesh-Portfolio/src/components/Header.jsx
+++ Lokesh-Portfolio/src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { Switch } from "@material-tailwind/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-regular-svg-icons/faMoon";
+import { useState, useEffect } from "react";
 
 function Header() {
+  const [DarkMode, SetDarkMode] = useState(() => {
+    const darkLocal = localStorage.getItem("DarkMode");
+    return darkLocal ? JSON.parse(darkLocal) : true;
+  });
+
+  const handleDark = () => {
+    SetDarkMode(!DarkMode);
+  };
+  useEffect(() => {
+    document.body.className = DarkMode ? "dark" : "light";
+    localStorage.setItem("DarkMode", JSON.stringify(DarkMode));
+  }, [DarkMode]);
   return (
     <div className="flex justify-between items-center px-8 pr-16 bg-header-black shadow-high-shadow">
       <div className="py-4">
@@ -34,13 +45,14 @@ function Header() {
       <Switch
         id="custom-switch-component"
         ripple={false}
-        className="h-full w-full checked:bg-[#2ec946]"
+        className="h-full w-full bg-[#37474f] checked:bg-[#D9D9D9]"
         containerProps={{
           className: "w-11 h-6",
         }}
         circleProps={{
           className: "before:hidden left-0.5 border-none",
         }}
+        onClick={handleDark}
       />
     </div>
   );
