70bd4eb2ad852b4b3c9e876c79aa1cc149d86d87
LokeshSr03 <lokeshsuthar0123@gmail.com>
2024-06-05 13:29:58 +0530

    data file created

---
+++ Lokesh-Portfolio/src/data/constants.js
@@ -0,0 +1,10 @@
+export const Bio = {
+  name: "Lokesh Suthar",
+  role: "Full Stack Developer",
+  github: "https://github.com/LokeshSr03",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/lokeshsuthar03/",
+  insta: "https://www.instagram.com/lokesh.sr.03/",
+  facebook: "https://www.facebook.com/lokesh.suthar.73744",
+};
