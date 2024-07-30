c2e19ce0fb4f4a63899b0fbeaa04b2678a0ee70d
LokeshSr03 <lokeshsuthar0123@gmail.com>
2024-07-30 12:07:10 +0530

    changes

--- Lokesh-Portfolio/src/data/constants.js
+++ Lokesh-Portfolio/src/data/constants.js
@@ -265,29 +265,24 @@ export const Project = [
     Pgithub: "#",
     Pgallery: [
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        imgelink: "/Images/port/p1.png",
       },
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        imgelink: "/Images/port/p2.png",
       },
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+        imgelink: "/Images/port/p3.png",
       },
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+        imgelink: "/Images/port/p4.png",
       },
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+        imgelink: "/Images/port/p5.png",
       },
     ],
   },
   {
-    Pimage: "/Images/class/rstclass.png",
+    Pimage: "/Images/E-waste/e-main.jpg",
     Pname: "E-Waste Management",
     Picon: [
       {
@@ -322,24 +317,19 @@ export const Project = [
     Pgithub: "#",
     Pgallery: [
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        imgelink: "/Images/E-waste/e1.jpg",
       },
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        imgelink: "/Images/E-waste/e2.jpg",
       },
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+        imgelink: "/Images/E-waste/e3.jpg",
       },
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+        imgelink: "/Images/E-waste/e4.jpg",
       },
       {
-        imgelink:
-          "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+        imgelink: "/Images/E-waste/e5.jpg",
       },
     ],
   },
